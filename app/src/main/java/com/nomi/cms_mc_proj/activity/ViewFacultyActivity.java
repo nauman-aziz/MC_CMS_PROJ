bd18d4b1b94025a99042c8b81b258a65e1d404d4
Nauman Aziz <m.nauman.aziz.1122@gmail.com>
2022-01-14 23:52:02 +0500

    view faculty activity added

---
+++ app/src/main/java/com/nomi/cms_mc_proj/activity/ViewFacultyActivity.java
@@ -0,0 +1,17 @@
+package com.nomi.cms_mc_proj.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nomi.cms_mc_proj.R;
+
+public class ViewFacultyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_faculty);
+    }
+
+}
