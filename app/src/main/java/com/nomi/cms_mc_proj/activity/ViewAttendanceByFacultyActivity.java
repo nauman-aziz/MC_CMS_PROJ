df73d3d2e2a3501a9fcb01a444ae5d2a2f275cfc
Nauman Aziz <m.nauman.aziz.1122@gmail.com>
2022-01-22 23:17:17 +0500

    code clear

--- app/src/main/java/com/nomi/cms_mc_proj/activity/ViewAttendanceByFacultyActivity.java
+++ app/src/main/java/com/nomi/cms_mc_proj/activity/ViewAttendanceByFacultyActivity.java
@@ -58,61 +58,6 @@ public class ViewAttendanceByFacultyActivity extends Activity {
 
 
 
-		/*listView.setOnItemLongClickListener(new OnItemLongClickListener() {
-
-			@Override
-			public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
-					final int position, long arg3) {
-
-
-
-				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ViewAttendanceByFacultyActivity.this);
-
-				alertDialogBuilder.setTitle(getTitle()+"decision");
-				alertDialogBuilder.setMessage("Are you sure?");
-
-				alertDialogBuilder.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog,int id) {
-
-						facultyList.remove(position);
-						listAdapter.notifyDataSetChanged();
-						listAdapter.notifyDataSetInvalidated();   
-
-						dbAdapter.deleteFaculty(facultyBeanList.get(position).getFaculty_id());
-						facultyBeanList=dbAdapter.getAllFaculty();
-
-						for(FacultyBean facultyBean : facultyBeanList)
-						{
-							String users = " FirstName: " + facultyBean.getFaculty_firstname()+"\nLastname:"+facultyBean.getFaculty_lastname();
-							facultyList.add(users);
-							Log.d("users: ", users); 
-
-						}
-						
-					}
-					
-				});
-				alertDialogBuilder.setNegativeButton("No",new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog,int id) {
-						// cancel the alert box and put a Toast to the user
-						dialog.cancel();
-						Toast.makeText(getApplicationContext(), "You choose cancel", 
-								Toast.LENGTH_LONG).show();
-					}
-				});
-
-				AlertDialog alertDialog = alertDialogBuilder.create();
-				// show alert
-				alertDialog.show();
-
-
-
-
-
-				return false;
-			}
-		});
-*/
 
 
 
